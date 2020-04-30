d4782547e6ad10752f1bc57fe570b170d417e26e
popper2710 <hige1332@yahoo.co.jp>
2020-04-30 15:04:35 +0900

    [add] add User gorm model

--- app/src/model.go
+++ app/src/model.go
@@ -1,11 +1,23 @@
 package src
 
-import "time"
+import (
+	"github.com/go-ozzo/ozzo-validation"
+	"github.com/go-ozzo/ozzo-validation/is"
+	"time"
+)
 
 type User struct {
-	Id        int       `gorm:"type:int(10);primary_key;auto_increment"`
+	Id        int       `gorm:"type:int AUTO_INCREMENT"`
 	Name      string    `gorm:"type:varchar(30);not null"`
+	Email     string    `gorm:"type:varchar(254);not null"`
 	Password  string    `gorm:"type:varchar(100);not null"`
 	LastLogin time.Time `gorm:"not null"`
 	CreateAt  time.Time `gorm:"not null"`
+}
+
+func (u User) Validate() error {
+	return validation.ValidateStruct(&u,
+		validation.Field(&u.Name, validation.Required, validation.Length(1, 30)),
+		validation.Field(&u.Email, validation.Required, is.Email),
+		validation.Field(&u.Password, validation.Required))
 }
