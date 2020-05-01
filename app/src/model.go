fd8cee048b79d2d1e29a0f424620c1354f57447d
popper2710 <hige1332@yahoo.co.jp>
2020-05-01 23:57:16 +0900

    [update] add uid column in User table

--- app/src/model.go
+++ app/src/model.go
@@ -7,7 +7,8 @@ import (
 )
 
 type User struct {
-	Id        int       `gorm:"type:int AUTO_INCREMENT"`
+	Id        int       `gorm:"type:int AUTO_INCREMENT unique"`
+	Uid       string    `gorm:"type:varchar(26);primary_key"`
 	Name      string    `gorm:"type:varchar(30);not null"`
 	Email     string    `gorm:"type:varchar(254);not null"`
 	Password  string    `gorm:"type:varchar(100);not null"`
@@ -17,6 +18,7 @@ type User struct {
 
 func (u User) Validate() error {
 	return validation.ValidateStruct(&u,
+		validation.Field(&u.Uid, validation.Required, validation.Length(26, 26)),
 		validation.Field(&u.Name, validation.Required, validation.Length(1, 30)),
 		validation.Field(&u.Email, validation.Required, is.Email),
 		validation.Field(&u.Password, validation.Required))
